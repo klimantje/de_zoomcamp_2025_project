00cfbba224d9ded627e987c7b7c26b22356227e3
An <an.derijdt@gmail.com>
2025-03-25 20:01:26 +0000

    wip

---
+++ upload_to_gcs.py
@@ -0,0 +1,21 @@
+from google.cloud import storage
+import os
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/workspaces/de_zoomcamp_2025_project/.creds/gcp_creds.json"
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    """
+    # # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    # storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    # storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+upload_to_gcs("bikes_rental_zc", "raw/2025_journeys.parquet",  "/workspaces/de_zoomcamp_2025_project/data/2025_journeys.parquet")
